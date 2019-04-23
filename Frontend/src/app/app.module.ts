d143d90e342cf5d70f56e64353084e59206af086
hgarner <hgarner@vater-gruppe.de>
2019-04-23 18:08:56 +0200

    Welcome Page

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,19 +1,19 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-import {HttpInterceptor} from '@angular/common/http';
-import {AppComponent} from './app.component';
-import {FormsModule} from '@angular/forms';
-import {AppRoutes} from './app. routes';
-import {CalendarComponent} from './components/calendar/calendar.component';
-import {CalendarModule, DateAdapter} from 'angular-calendar';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
-import {CompanyService} from './services/company.service';
-import {TimeEntryComponent} from './components/time-entry/time-entry.component';
-import {TimeEntryService} from './services/time-entry.service';
-import {TimeListComponent} from './components/time-list/time-list.component';
-import {CompaniesListComponent} from './components/companies-list/companies-list.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpInterceptor } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutes } from './app. routes';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CompanyService } from './services/company.service';
+import { TimeEntryComponent } from './components/time-entry/time-entry.component';
+import { TimeEntryService } from './services/time-entry.service';
+import { TimeListComponent } from './components/time-list/time-list.component';
+import { CompaniesListComponent } from './components/companies-list/companies-list.component';
 import {
   MatOptionModule,
   MatSelectModule,
@@ -24,19 +24,20 @@ import {
   MatNativeDateModule,
   MatTableModule,
   MatDividerModule,
-  MatSidenavModule,  MatGridListModule
+  MatSidenavModule, MatGridListModule
 } from '@angular/material';
-import {LoginComponent} from './components/login/login.component';
-import {MatButtonModule} from '@angular/material';
-import {TokenStorageService} from './services/token-storage.service';
-import {AuthInterceptor} from './auth/AuthInterceptor';
-import {LoginCommunicationService} from './services/login-communication.service';
-import {AuthGuard} from './auth/AuthGuard';
-import {LogoutComponent} from './components/logout/logout.component';
-import {UserStorageService} from './services/user-storage.service';
-import {ControllingComponent} from './components/controlling/controlling.component';
-import {TimeListChildDeleteComponent} from './components/time-list/time-list-child-delete/time-list-child-delete.component';
-import {TimeEntryChangeComponent} from './components/time-list/time-entry-change/time-entry-change.component';
+import { LoginComponent } from './components/login/login.component';
+import { MatButtonModule } from '@angular/material';
+import { TokenStorageService } from './services/token-storage.service';
+import { AuthInterceptor } from './auth/AuthInterceptor';
+import { LoginCommunicationService } from './services/login-communication.service';
+import { AuthGuard } from './auth/AuthGuard';
+import { LogoutComponent } from './components/logout/logout.component';
+import { UserStorageService } from './services/user-storage.service';
+import { ControllingComponent } from './components/controlling/controlling.component';
+import { TimeListChildDeleteComponent } from './components/time-list/time-list-child-delete/time-list-child-delete.component';
+import { TimeEntryChangeComponent } from './components/time-list/time-entry-change/time-entry-change.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
@@ -50,6 +51,7 @@ import {TimeEntryChangeComponent} from './components/time-list/time-entry-change
     ControllingComponent,
     TimeListChildDeleteComponent,
     TimeEntryChangeComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
@@ -81,7 +83,7 @@ import {TimeEntryChangeComponent} from './components/time-list/time-entry-change
     CompanyService,
     UserStorageService,
     TimeEntryService,
-    TokenStorageService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    TokenStorageService, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     LoginCommunicationService],
   bootstrap: [AppComponent]
 })
